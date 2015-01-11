ae89ae4ee48c82bcc48225c41f6f834129c78d36
zhouhuakang <zhouhuakang459@gmail.com>
2015-01-11 12:54:45 +0800

    1.Realize the file server client 2 Add exercise_2_common_erlang_code

---
+++ exercise_2_common_erlang_code/src/common_code.erl
@@ -0,0 +1,17 @@
+%%%-------------------------------------------------------------------
+%%% @author zhk
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 11. Jan 2015 12:51 PM
+%%%-------------------------------------------------------------------
+-module(common_code).
+-author("zhk").
+
+%% API
+-export([]).
+%% 1. X is even,return true,else return false
+Even = fun(X) -> (X rem 2 ) =:= 0 end.
+
+
